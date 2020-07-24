ab4a51b5e93fea2bf12471caf0c24dc30ac10c22
Carter Parrett <cparrett94@hotmail.com>
2020-07-24 20:39:50 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,7 +4,7 @@
 
 class GetRequester
   
-  url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+  URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
   
   attr_accessor :url 
   
@@ -13,7 +13,7 @@ class GetRequester
   end
   
   def get_response_body
-    uri = URI.parse(url)
+    uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
   end
